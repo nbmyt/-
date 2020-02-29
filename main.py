ab43bf6af40219f7f135343861aa1d6af8137a1a
w <wzwsy2013@outlook.com>
2020-02-29 12:11:45 +0800

    修复了空格键影响球速, 修改了第一关地图, 修改了背景图片、砖块图片

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import traceback
 import board
 import ball
 import brick
+import health
 import reward
 from pygame.locals import *
 
@@ -11,28 +12,35 @@ pygame.init()
 pygame.mixer.init()
 
 # 设置帧率
-FPS = 30
+FPS = 60
 
 # 初始化数据
 bg_size = width, height = 860, 573
-screen = pygame.display.set_mode(bg_size, RESIZABLE)
+screen = pygame.display.set_mode(bg_size)
 pygame.display.set_caption("打砖块")
+icon = pygame.image.load("images/icon.ico")
+pygame.display.set_icon(icon)
 background = pygame.image.load("images/第一关.jpg").convert()
 position = background.get_rect()
-brick_position = [5, 10]
+brick_position = [200, 10]
+health_position = [573, 860]
 reward_speed = [0, 2]
 filename = 'map.txt'
 clock = pygame.time.Clock()
 
 # 加载背景音乐
-pygame.mixer.music.load("sounds/illusion.ogg")
-pygame.mixer.music.set_volume(0.2)
+pygame.mixer.init()
+pygame.mixer.init()
+ball_brick_sound= pygame.mixer.Sound("sounds/撞击砖块1.wav")
+ball_board_sound = pygame.mixer.Sound("sounds/撞击挡板.wav")
+ball_wall_sound = pygame.mixer.Sound("sounds/撞击墙壁.wav")
 
 # 生成动画精灵
 group_brick = pygame.sprite.Group()
 group_ball = pygame.sprite.Group()
 group_board = pygame.sprite.Group()
 group_reward = pygame.sprite.Group()
+group_healths = pygame.sprite.Group()
 
 # 读取地图
 mapFile = open(filename, 'r')
@@ -56,12 +64,18 @@ def creat_brick():
             if each == '3':
                 bricks.curimage = bricks.image3
             group_brick.add(bricks)
-            brick_position[0] += 50
-        brick_position[0] = 5
-        brick_position[1] += 40
-    brick_position[1] = 10
+            brick_position[0] += 16
+        brick_position[0] = 200
+        brick_position[1] += 16
+    brick_position[1] = 20
 
-
+#初始化生命值
+def creat_health():
+    for num in range(3):
+        healths = health.Health()
+        healths.change(health_position)
+        group_healths.add(healths)
+        health_position[1] -= 60
 # 小球反弹木板方向判断
 def board_collide(ball_position, board_position, ball_speed):
     # ↘方向反弹
@@ -105,7 +119,10 @@ def board_collide(ball_position, board_position, ball_speed):
 def brick_collide(ball_position, brick_position, ball_speed):
     # ↗方向反弹
     if ball_speed[0] > 0 and ball_speed[1] < 0:
-        angle = (ball_position[1] - (brick_position[1] + 22)) / (ball_position[0] + 15 - brick_position[0])
+        if ball_position[0] + 17 - brick_position[0] == 0:
+            angle = -1
+        else:
+            angle = (ball_position[1] - (brick_position[1] + 22)) / (ball_position[0] + 17 - brick_position[0])
         if angle <= -1.0:
             return False
         else:
@@ -119,7 +136,10 @@ def brick_collide(ball_position, brick_position, ball_speed):
             return True
     # ↘方向反弹
     elif ball_speed[0] > 0 and ball_speed[1] > 0:
-        angle = (ball_position[1] + 15 - brick_position[1]) / (ball_position[0] + 15 - brick_position[0])
+        if ball_position[0] + 17 - brick_position[0] == 0:
+            angle = 1
+        else:
+            angle = (ball_position[1] + 16 - brick_position[1]) / (ball_position[0] + 17 - brick_position[0])
         if angle >= 1.0:
             return False
         else:
@@ -135,18 +155,22 @@ def brick_collide(ball_position, brick_position, ball_speed):
 
 def main():
     score = 0
+    is_remove = False
     running = True
-    moving = False
     is_invincible = False
     is_lengthenboard = False
     score_font = pygame.font.Font("font/font.ttf", 36)
     creat_brick()
+    creat_health()
 
     # 生成动画精灵
-    boards = board.Board(bg_size) #生成挡板
+    boards = board.Board(bg_size)  # 生成挡板
     group_board.add(boards)
-    balls = ball.Ball(bg_size) #生成球
+    balls = ball.Ball(boards)  # 生成球
+    balls.ball_speed = [0, 0]
+    is_moving = False
     group_ball.add(balls)
+
 
     # 定义计时器
     INVINCIBLE_TIME = USEREVENT #24
@@ -154,7 +178,7 @@ def main():
     SPEEDUPBALL_TIME = USEREVENT + 2
 
     # 设置循环播放
-    pygame.mixer.music.play(-1)
+#    pygame.mixer.music.play(-1)
 
     # 开始主循环
     while running:
@@ -162,21 +186,15 @@ def main():
             if event.type == QUIT:
                 #pygame.quit()
                 sys.exit()
-            if event.type == MOUSEBUTTONDOWN:
-                if event.button == 1:
-                    # 仅当鼠标在板内方可移动
-                    mouse_position = pygame.mouse.get_pos()
-                    if boards.rect[0] < mouse_position[0] < boards.rect[0] + boards.rect[2] and \
-                                            boards.rect[1] < mouse_position[1] < boards.rect[1] + boards.rect[3]:
-                        moving = True
-            if event.type == MOUSEBUTTONUP:
-                if event.button == 1:
-                    moving = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE and not is_moving:
+                    is_moving = True
+                    balls.ball_speed = [3,-3]
             if event.type == INVINCIBLE_TIME:
                 is_invincible = False
                 pygame.time.set_timer(INVINCIBLE_TIME, 0)
             if event.type == LENGTHENBOARD_TIME:
-                boards.change_object()
+                boards.change_object(boards.rect.left)
                 is_lengthenboard = False
                 pygame.time.set_timer(LENGTHENBOARD_TIME, 0)
             # 球速恢复
@@ -189,14 +207,15 @@ def main():
 
         screen.blit(background, (0, 0))
 
+
         # 板与加长板的互相转化
-        if moving:
-            mouse_position = pygame.mouse.get_pos()
-            if 450 <= mouse_position[1] <= 573 and 800 >= mouse_position[0] >= 50 and not is_lengthenboard:
-                #boards.rect = (mouse_position[0] - 62, 435, boards.rect[2], boards.rect[3])
-                boards.rect = boards.rect.move(mouse_position[0] - boards.rect[0], 0)
-            elif 450 <= mouse_position[1] <= 573 and 825 >= mouse_position[0] >= 65 and is_lengthenboard:
-                boards.rect = (mouse_position[0] - 90, 535, boards.rect[2], boards.rect[3])
+        boardSpeed = 0
+        if event.type == pygame.KEYDOWN and is_moving:
+            if event.key == pygame.K_LEFT:
+                boardSpeed = -8 if boards.rect.left   > 0 else 0
+            elif event.key == pygame.K_RIGHT:
+                boardSpeed = 8 if boards.rect.right  < width else 0
+            boards.rect.left += boardSpeed
             group_board.add(boards)
 
         if not is_lengthenboard:
@@ -215,11 +234,13 @@ def main():
 
             # 检测球与边界的碰撞
             if balls.rect.left < 0:
+                ball_wall_sound.play()
                 # 碰撞后赋值为0，防止反弹后仍然碰撞从而卡在边界上
                 balls.rect.left = 0
                 balls.ball_speed[0] = -balls.ball_speed[0]
                 balls.is_hit = False
             elif balls.rect.right > width:
+                ball_wall_sound.play()
                 balls.rect.right = width
                 balls.ball_speed[0] = -balls.ball_speed[0]
                 balls.is_hit = False
@@ -230,7 +251,22 @@ def main():
                     balls.is_hit = False
                 elif not is_invincible:
                     group_ball.remove(balls)
+                    if len(group_ball ) == 0:
+                        if len(group_healths) > 1:
+                            is_remove = True
+                            balls = ball.Ball(boards)
+                            group_ball.add(balls)
+                            is_moving = False
+                            balls.ball_speed = [0, 0]
+                            group_board.remove(boards)
+                            group_board.add(boards)
+                        else:
+                            sys.exit()
+                #balls.rect.bottom = height
+                balls.ball_speed[1] = -balls.ball_speed[1]
+                balls.is_hit = False
             elif balls.rect.top < 0:
+                ball_wall_sound.play()
                 balls.rect.top = 0
                 balls.ball_speed[1] = -balls.ball_speed[1]
                 balls.is_hit = False
@@ -242,6 +278,7 @@ def main():
                 # 小球反弹木板碰撞方向检测
                 board_hit_top = board_collide(balls.rect, boards.rect, balls.ball_speed)
                 if not balls.is_hit:
+                    ball_wall_sound.play()
                     if not board_hit_top:
                         balls.ball_speed[0] = -balls.ball_speed[0]
                         balls.ball_speed[1] = -balls.ball_speed[1]
@@ -258,9 +295,13 @@ def main():
                         balls.ball_speed[0] = -balls.ball_speed[0]
                     else:
                         balls.ball_speed[1] = -balls.ball_speed[1]
+                    ball_brick_sound.play()
                     if each.curimage == each.image1:
                         score += 10
                         group_brick.remove(each)
+                        if len(group_brick) == 0:
+                            pass
+                        #new_game
                     elif each.curimage == each.image2:
                         score += 20
                         each.curimage = each.image1
@@ -276,6 +317,7 @@ def main():
 
             # 移动小球
             balls.rect = balls.rect.move(balls.ball_speed)
+
             screen.blit(balls.image1, balls.rect)
 
         # 奖励机制
@@ -289,12 +331,12 @@ def main():
                 # 板加长(10s)
                 elif rewards.current_image == rewards.image2:
                     if is_lengthenboard == False:
-                        boards.change_object()
+                        boards.change_object(boards.rect.left)
                         is_lengthenboard = True
                     pygame.time.set_timer(LENGTHENBOARD_TIME, 10 * 1000)
                 # 球数+1
                 elif rewards.current_image == rewards.image3:
-                    balls = ball.Ball(bg_size)
+                    balls = ball.Ball(boards)
                     group_ball.add(balls)
                 # 球速度+5(10s)
                 elif rewards.current_image == rewards.image4:
@@ -308,6 +350,12 @@ def main():
             screen.blit(rewards.current_image, rewards.rect)
             rewards.rect = rewards.rect.move(reward_speed)
 
+        #画生命值
+        for healths in group_healths:
+            if is_remove:
+                group_healths.remove(healths)
+                is_remove = False
+            screen.blit(healths.image, healths.rect)
         # 画砖块
         for bricks in group_brick:
             if bricks.curimage == bricks.image4:
